f528c91e8afe20995b0953c57bbfc183a07bbe71
prg-04 <karanjaevans95@gmail.com>
2023-03-28 07:15:11 +0300

    Add dev-dependencies and testing packages

---
+++ script.js
@@ -0,0 +1,8 @@
+let menuIcon = document.querySelector(".fa-bars-staggered");
+let sidebar = document.querySelector(".sidebar");
+let laContainer = document.querySelector(".laContainer");
+
+menuIcon.onclick = function () {
+  sidebar.classList.toggle("small-sidebar");
+  laContainer.classList.toggle("large-container");
+};
